2c4150940bb95297dbdbe9eaff15eb011dfbb410
Rahaman <shaikjameelurrahaman@gmail.com>
2025-03-18 17:45:19 +0530

    Add Councilspage component and update .gitignore

---
+++ frontend/src/Councilspage/components_Councilspage/Councilspage.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header_councilspage from "./Header_councilspage";
+import Body_councilspage from "./Body_councilspage";
+
+function Councilspage() {
+    return (
+        <div className="councilspage_maindiv-0">
+            <div className="councilspage_maindiv-1">
+                <div className="councilspage_maindiv-2">
+                    <div className="councilspage_maindiv-3">
+                        <Header_councilspage />
+                        <Body_councilspage />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Councilspage;
